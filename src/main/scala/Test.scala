8fd696f730482857c7925c1431591ef02b9cbaa7
FireFry <v.a.burakov@gmail.com>
2014-10-14 13:28:43 +0300

    Add parser for profile page to extract author name and accepted problems

--- src/main/scala/Test.scala
+++ src/main/scala/Test.scala
@@ -3,5 +3,7 @@ import scala.App
 
 object Test extends App {
   val id = Integer.parseInt(System.getProperty("author"))
-  print(Timus.profile(Author(id)))
+  Timus.profile(Author(id)).map {
+    println
+  }
 }
--- src/main/scala/com/burakovv/timus/Timus.scala
+++ src/main/scala/com/burakovv/timus/Timus.scala
@@ -2,22 +2,30 @@ package com.burakovv.timus
 
 import org.jsoup.Jsoup
 import org.jsoup.nodes.{Node, Document, TextNode}
+import scala.collection.immutable
 
 object Timus {
-  def profile(author: Author): Option[Profile] = {
-    val url: String = s"http://acm.timus.ru/author.aspx?id=${author.id}"
-    Profile.parse(Jsoup.connect(url).get())
-  }
+  def profile(author: Author): Option[Profile] = Profile.parse(loadPage(profileUrl(author)))
+  def loadPage(url: String): Document = Jsoup.connect(url).get()
+  def profileUrl(author: Author): String = s"http://acm.timus.ru/author.aspx?id=${author.id}"
 }
 
 case class Author(id: Int)
 
-class Profile private[timus]() {
-}
+case class Problem(id: Int)
+
+case class Profile private[timus](name: String, accepts: immutable.Seq[Problem])
 
 private[timus] object Profile {
   def parse(document: Document): Option[Profile] = isKnownAuthor(document) match {
-    case true => Some(new Profile { override def toString: String = document.toString })
+    case true => {
+      val name = document.getElementsByClass("author_name").text()
+      val accepts = {
+        import scala.collection.JavaConversions._
+        document.getElementsByClass("accepted").toIndexedSeq.map(el => Problem(el.child(0).text().toInt))
+      }
+      Some(new Profile(name, accepts))
+    }
     case false => None
   }
 
