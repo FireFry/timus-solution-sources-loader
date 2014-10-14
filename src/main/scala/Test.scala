42cadf1ae85c894037ebc31feee19dece03a68f0
FireFry <v.a.burakov@gmail.com>
2014-10-14 17:24:15 +0300

    Make it work. Succeeded to load best solutions from server.

--- src/main/scala/Test.scala
+++ src/main/scala/Test.scala
@@ -1,19 +1,29 @@
 import com.burakovv.timus._
+import java.nio.charset.StandardCharsets
+import java.nio.file.{Files, Paths}
 import scala.App
 
 object Test extends App {
   val id = Integer.parseInt(System.getProperty("author"))
   val login = System.getProperty("login")
   val password = System.getProperty("password")
+  val dir = System.getProperty("outputDir")
+  var sources = 0
 
-  def findBest(submissions: Seq[Submission]) = ???
+  def findBest(submissions: Seq[Submission]): Option[Submission] = Some(submissions.foldLeft(submissions(0))((min: Submission, s: Submission) => {
+    if (s.accepted && s.runtime < min.runtime) s else min
+  }))
 
-  def saveSource(source: Source) = ???
+  def saveSource(problem: Problem, submission: Submission, source: Source) = {
+    sources += 1
+    println(s"Saving source $sources for $submission")
+    Files.write(Paths.get(s"${dir}${problem.id}.${submission.langExtension}"), source.text.getBytes(StandardCharsets.UTF_8))
+  }
 
   for {
     profile <- Timus.profile(Author(id))
     problem <- profile.problems if problem.status == ProblemStatus.Accepted
     bestTry <- findBest(Timus.submissions(problem))
     source <- Timus.source(bestTry, Credentials(login, password))
-  } saveSource(source)
+  } saveSource(problem, bestTry, source)
 }
--- src/main/scala/com/burakovv/timus/Timus.scala
+++ src/main/scala/com/burakovv/timus/Timus.scala
@@ -1,10 +1,11 @@
 package com.burakovv.timus
 
 import org.jsoup.{Connection, Jsoup}
-import org.jsoup.nodes.{Node, Document, TextNode}
+import org.jsoup.nodes.{Element, Node, Document, TextNode}
 import scala.collection.immutable
 import scala.collection.mutable.ArrayBuffer
 import com.burakovv.timus.ProblemStatus.ProblemStatus
+import org.jsoup.select.Elements
 
 object Timus {
   def profile(author: Author): Option[Profile] = Profile parse {
@@ -13,25 +14,29 @@ object Timus {
       get()
   }
 
-  def submissions(problem: Problem): Seq[Submission] = ???
-
-  def source(submission: Submission, credentials: Credentials): Source = Source {
+  def submissions(problem: Problem): Seq[Submission] = Submission parse {
     Jsoup.
-      connect(s"http://acm.timus.ru/getsubmit.aspx/${submission.id}.java").
-      data("Action", "getsubmit").
-      data("JudgeID", credentials.login).
-      data("Password", credentials.password).
-      method(Connection.Method.POST).
-      execute().
-      body()
+      connect(s"http://acm.timus.ru/status.aspx?space=1&num=${problem.id}&author=42165&count=100").
+      get()
+  }
+
+  def source(submission: Submission, credentials: Credentials): Option[Source] = Some {
+    Source {
+      Jsoup.
+        connect(s"http://acm.timus.ru/getsubmit.aspx/${submission.id}.${submission.langExtension}").
+        data("Action", "getsubmit").
+        data("JudgeID", credentials.login).
+        data("Password", credentials.password).
+        method(Connection.Method.POST).
+        execute().
+        body()
+    }
   }
 }
 
 case class Author(id: Int)
 
 case class Problem(id: Int, status: ProblemStatus)
-
-case class Profile private[timus](name: String, problems: immutable.Seq[Problem])
 
 object ProblemStatus extends Enumeration {
   type ProblemStatus = Value
@@ -40,13 +45,13 @@ object ProblemStatus extends Enumeration {
 
 case class Credentials(login: String, password: String)
 
-case class Submission(accepted: Boolean, id: Int, langExtension: String, runtime: Double)
-
 case class Source(text: String)
 
-private[timus] object Profile {
-  def parse(document: Document): Option[Profile] = isKnownAuthor(document) match {
-    case true => {
+case class Profile private[timus](name: String, problems: immutable.Seq[Problem])
+
+object Profile {
+  private[timus] def parse(document: Document): Option[Profile] = isKnownAuthor(document) match {
+    case true =>
       val name = document.getElementsByClass("author_name").text()
       val problems = {
         import scala.collection.JavaConversions._
@@ -64,7 +69,6 @@ private[timus] object Profile {
         builder.toIndexedSeq
       }
       Some(new Profile(name, problems))
-    }
     case false => None
   }
 
@@ -74,5 +78,28 @@ private[timus] object Profile {
       case t: TextNode if t.text == "Author not found" => false
       case _ => true
     }
+  }
+}
+
+case class Submission(accepted: Boolean, id: Int, langExtension: String, runtime: Double)
+
+object Submission {
+
+  private[timus] def parse(document: Document): Seq[Submission] = {
+    import scala.collection.JavaConversions._
+
+    def parseId(elements: Element): Int = elements.child(0).text().toInt
+
+    def parseLangExtension(element: Element): String = element.child(0).attr("href").split('.').last
+
+    def toOption(elements: Elements): Option[Element] = if (elements.size == 0) None else Some(elements.get(0))
+
+    def parseSubmission(el: Element): Option[Submission] = for {
+      id <- toOption(el.getElementsByClass("id"))
+      runtime <- toOption(el.getElementsByClass("runtime"))
+      verdict <- toOption(el.getElementsByClass("verdict_ac")) orElse toOption(el.getElementsByClass("verdict_rj"))
+    } yield Submission(verdict.hasClass("verdict_ac"), parseId(id), parseLangExtension(id), if (runtime.text.isEmpty) 0d else runtime.text.toDouble)
+
+    document.getElementsByClass("status").get(0).child(0).children().flatMap(parseSubmission)
   }
 }
