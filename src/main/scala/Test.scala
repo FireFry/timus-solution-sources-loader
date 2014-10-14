82f2aba1903e7d18ba43bd512fe19818b94e1564
FireFry <v.a.burakov@gmail.com>
2014-10-14 14:28:16 +0300

    Refactoring. Complete Test.scala main body without implementing. Make ProblemStatus enumeration.

--- src/main/scala/Test.scala
+++ src/main/scala/Test.scala
@@ -1,9 +1,15 @@
-import com.burakovv.timus.{Author, Timus}
+import com.burakovv.timus._
 import scala.App
 
 object Test extends App {
   val id = Integer.parseInt(System.getProperty("author"))
-  Timus.profile(Author(id)).map {
-    println
-  }
+  val login = Integer.parseInt(System.getProperty("login"))
+  val password = Integer.parseInt(System.getProperty("password"))
+
+  for {
+    profile <- Timus.profile(Author(id))
+    problem <- profile.problems if problem.status == ProblemStatus.Accepted
+    bestTry <- findBest(Timus.submissions(problem))
+    source <- Timus.source(bestTry, Credentials(login, password))
+  } saveSource(source)
 }
--- src/main/scala/com/burakovv/timus/Timus.scala
+++ src/main/scala/com/burakovv/timus/Timus.scala
@@ -3,6 +3,8 @@ package com.burakovv.timus
 import org.jsoup.Jsoup
 import org.jsoup.nodes.{Node, Document, TextNode}
 import scala.collection.immutable
+import scala.collection.mutable.ArrayBuffer
+import com.burakovv.timus.ProblemStatus.ProblemStatus
 
 object Timus {
   def profile(author: Author): Option[Profile] = Profile.parse(loadPage(profileUrl(author)))
@@ -12,19 +14,35 @@ object Timus {
 
 case class Author(id: Int)
 
-case class Problem(id: Int)
+case class Problem(id: Int, status: ProblemStatus)
 
-case class Profile private[timus](name: String, accepts: immutable.Seq[Problem])
+case class Profile private[timus](name: String, problems: immutable.Seq[Problem])
+
+object ProblemStatus extends Enumeration {
+  type ProblemStatus = Value
+  val Accepted, Failed, Untouched = Value
+}
 
 private[timus] object Profile {
   def parse(document: Document): Option[Profile] = isKnownAuthor(document) match {
     case true => {
       val name = document.getElementsByClass("author_name").text()
-      val accepts = {
+      val problems = {
         import scala.collection.JavaConversions._
-        document.getElementsByClass("accepted").toIndexedSeq.map(el => Problem(el.child(0).text().toInt))
+        import com.burakovv.timus.ProblemStatus._
+
+        val builder = new ArrayBuffer[Problem]()
+
+        def parseProblems(elementClass: String, status: ProblemStatus) =
+          document.getElementsByClass(elementClass).map(e => Problem(e.child(0).text().toInt, status))
+
+        builder ++= parseProblems("accepted", Accepted)
+        builder ++= parseProblems("tried", Failed)
+        builder ++= parseProblems("empty", Untouched)
+
+        builder.toIndexedSeq
       }
-      Some(new Profile(name, accepts))
+      Some(new Profile(name, problems))
     }
     case false => None
   }
