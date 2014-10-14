5eac404fd5d94e081520a0fbb0293b39ed791d31
FireFry <v.a.burakov@gmail.com>
2014-10-14 12:09:50 +0300

    Refactoring

--- src/main/scala/com/burakovv/timus/Timus.scala
+++ src/main/scala/com/burakovv/timus/Timus.scala
@@ -1,23 +1,31 @@
 package com.burakovv.timus
 
 import org.jsoup.Jsoup
+import org.jsoup.nodes.{Node, Document, TextNode}
 
 object Timus {
-  def profile(author: Author) = new Profile(author)
+  def profile(author: Author): Option[Profile] = {
+    val url: String = s"http://acm.timus.ru/author.aspx?id=${author.id}"
+    Profile.parse(Jsoup.connect(url).get())
+  }
 }
 
-case class Author(id: Int) {
-  override def toString: String = String.valueOf(id)
+case class Author(id: Int)
+
+class Profile private[timus]() {
 }
 
-class Profile private[timus] (author: Author) extends JsoupConnection {
-  override val documentUrl = s"http://acm.timus.ru/author.aspx?id=$author"
-}
+private[timus] object Profile {
+  def parse(document: Document): Option[Profile] = isKnownAuthor(document) match {
+    case true => Some(new Profile { override def toString: String = document.toString })
+    case false => None
+  }
 
-private[timus] trait JsoupConnection {
-  private[timus] lazy val document = loadDocument()
-  private[timus] def loadDocument() = Jsoup.connect(documentUrl).get()
-  private[timus] def documentUrl: String
-
-  override def toString: String = document.toString
+  private def isKnownAuthor(profilePage: Document) = {
+    val childNode: Node = profilePage.body().child(0).child(0).child(2).child(0).child(0).childNode(0)
+    childNode match {
+      case t: TextNode if t.text == "Author not found" => false
+      case _ => true
+    }
+  }
 }
